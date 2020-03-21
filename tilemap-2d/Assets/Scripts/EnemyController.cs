b1211a7ded79633d53c1392665c59c836fe293c6
gustavo-Fuentes <fuentesfei99@gmail.com>
2020-03-21 19:33:51 -0300

    .

---
+++ tilemap-2d/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    protected int health;
+    public float distanceAttack;
+    public float speed;
+    protected bool isMoving = false;
+
+    protected Rigidbody2D rb2d;
+    protected Animator anim;
+    protected Transform player;
+    protected SpriteRenderer sprite;
+
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player").GetComponent<Transform> ();
+    }
+
+    protected float PlayerDistance()
+    {
+        return Vector2.Distance(player.position, transform.position);
+    }
+
+    protected void Flip()
+    {
+        sprite.flipX = !sprite.flipX;
+        speed *= -1;
+    }
+}
